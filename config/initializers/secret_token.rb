d996c0814b464b3fc6c378b876b24e65314bb180
Ronis Gracie Pereira <ronis@gracieoc.com>
2013-08-21 14:49:15 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ronisrails::Application.config.secret_token = 'c4b049977d2ed8733f57fc6536fa7e7a7f41fd6eafc2eb65fc0fb850a4579a4c93961e72fbd4e0e38b06b340353d61cf362a54f065cecb100d828232762cf111'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ronisrails::Application.config.session_store :cookie_store, key: '_ronisrails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ronisrails::Application.config.session_store :active_record_store
